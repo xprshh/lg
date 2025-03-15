e30b7d7eaa0c8e7a378f33add18670550ee095e2
xpr <a.mameter@proton.me>
2025-03-15 15:28:27 -0400

    boom

--- nixos/steam.nix
+++ nixos/steam.nix
@@ -1,11 +1,8 @@
 { config, ... }: {
   programs = {
- donf.enable = true;
     steam = {
-      enable = true; # i use steam for light gaming but will get a gaming laptop in the feature so i can heavy gaming
+      enable = true; # I use Steam for light gaming but will get a gaming laptop in the future for heavy gaming
       gamescopeSession.enable = config.programs.gamescope.enable;
     };
-
-    gamescope.enable = true;
   };
 }
